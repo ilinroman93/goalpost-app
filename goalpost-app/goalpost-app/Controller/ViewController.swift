adf03b78ff86737752067de4dd1bde9cb47ba3a1
Roman Ilin <ilrose887@gmail.com>
2018-02-03 00:07:22 +0800

    project initialized

---
+++ goalpost-app/goalpost-app/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  goalpost-app
+//
+//  Created by Ильин Роман on 03.02.2018.
+//  Copyright © 2018 Romax. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
