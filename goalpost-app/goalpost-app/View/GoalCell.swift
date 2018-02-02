2b4799d14bfb726dcd44481965a82367f3e8220b
Roman Ilin <ilrose887@gmail.com>
2018-02-03 01:00:54 +0800

    some tableviewcell stuff is done

---
+++ goalpost-app/goalpost-app/View/GoalCell.swift
@@ -0,0 +1,22 @@
+//
+//  GoalCell.swift
+//  goalpost-app
+//
+//  Created by Ильин Роман on 03.02.2018.
+//  Copyright © 2018 Romax. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+}
