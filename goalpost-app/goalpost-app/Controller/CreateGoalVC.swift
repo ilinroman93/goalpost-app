e19cf722ba1b0432f0dc695958a78c473e270a83
Roman Ilin <ilrose887@gmail.com>
2018-02-03 13:15:17 +0800

    create goal VC created and core data initialized

---
+++ goalpost-app/goalpost-app/Controller/CreateGoalVC.swift
@@ -0,0 +1,44 @@
+//
+//  CreateGoalVC.swift
+//  goalpost-app
+//
+//  Created by Ильин Роман on 03.02.2018.
+//  Copyright © 2018 Romax. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func nextBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func shortTermBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func longTermBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+}
---
+++ goalpost-app/goalpost-app/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  goalpost-app
+//
+//  Created by Ильин Роман on 03.02.2018.
+//  Copyright © 2018 Romax. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
